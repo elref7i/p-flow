77d5c6aa4c4e2874418b925ad6c9949f54e8c5ea
elref7i <momo3956248@gmail.com>
2025-02-12 09:34:40 +0200

    Update Home component styling; enhance Footer for fixed positioning; modify Layout to conditionally render Outlet based on authentication pages

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import Navbar from '@/components/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import Footer from '../components/Footer/Footer';
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -7,6 +7,16 @@ import { useContext } from 'react';
 import { UserTypeContext } from '../context/UserType.context';
 export default function Layout() {
   const { token } = useContext(UserTypeContext);
+  const { pathname } = useLocation();
+
+  const authPages = [
+    '/login',
+    '/signup',
+    '/forgetpassword',
+    '/updatedpassword',
+    '/verifysendcoding',
+    '/landing',
+  ];
   return (
     <>
       <Navbar />
@@ -15,14 +25,18 @@ export default function Layout() {
         component={'main'}
         sx={{
           m: 0,
-          pt: '100px',
-          minHeight: 'calc(100vh-86.01px)',
+          py: '30px',
+          minHeight: 'calc(100vh - 64px)',
           backgroundColor: 'background.default',
         }}
       >
-        <Container maxWidth="lg">
-          <Outlet></Outlet>
-        </Container>
+        {authPages.includes(pathname) ? (
+          <Outlet />
+        ) : (
+          <Container maxWidth="lg">
+            <Outlet />
+          </Container>
+        )}
       </Box>
       <Footer />
     </>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,19 @@ import { Box, Typography } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Box id="footer" sx={{ textAlign: 'center', py: 3, bgcolor: 'grey.200' }}>
-      <Typography variant="body2">
+    <Box
+      id="footer"
+      sx={{
+        textAlign: 'center',
+        py: 3,
+        bgcolor: 'grey.200',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        position: 'fixed',
+      }}
+    >
+      <Typography variant="body2 ">
         &copy; 2025 P-Flow. All rights reserved.
       </Typography>
     </Box>
