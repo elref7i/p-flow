87e3fdf7a7a220cf24ce432093e45e734ad27ddf
ziaaad920 <zyad16230@gmail.com>
2025-02-18 23:57:23 +0200

    Refactor Sidebar and UserType context for improved user data handling and update Login component for better password management

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,57 +5,57 @@ import {
   IconButton,
   Typography,
   useTheme,
-} from '@mui/material';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-import ImageAdmin from '@/assets/photo_2024-12-03_19-37-17.jpg';
-import { useThemeContext } from '../../context/theme.context';
-import { Drawer, DrawerHeader } from '../Common/Drawer';
-import SidebarSection from './SidebarSection';
-import { admin, inventory, pharmacy } from './DefaultItemes';
-import { useTypeContext } from '../../context/UserType.context';
+import ImageAdmin from "@/assets/photo_2024-12-03_19-37-17.jpg";
+import { useThemeContext } from "../../context/theme.context";
+import { Drawer, DrawerHeader } from "../Common/Drawer";
+import SidebarSection from "./SidebarSection";
+import { admin, inventory, pharmacy } from "./DefaultItemes";
+import { useTypeContext } from "../../context/UserType.context";
 
 export default function Sidebar() {
   const theme = useTheme();
   const { open, handleDrawerClose } = useThemeContext();
-  const { role } = useTypeContext();
+  const { role, userData } = useTypeContext();
 
   const { HeaderSection, MiddleSection, FooterSection } =
-    role === 'admin' ? admin : role === 'pharmacy' ? pharmacy : inventory;
+    role === "admin" ? admin : role === "pharmacy" ? pharmacy : inventory;
   return (
     <Drawer variant="permanent" open={open}>
       <DrawerHeader>
         <IconButton onClick={handleDrawerClose}>
-          {theme.direction === 'rtl' ? (
+          {theme.direction === "rtl" ? (
             <ChevronRightIcon />
           ) : (
             <ChevronLeftIcon />
           )}
         </IconButton>
       </DrawerHeader>
-      <Box textAlign={'center'} marginInline={open ? 2 : 0} marginBlock={2}>
+      <Box textAlign={"center"} marginInline={open ? 2 : 0} marginBlock={2}>
         <Avatar
           alt="Ahmed Refai"
           src={ImageAdmin}
           sx={{
             width: open ? 70 : 40,
             height: open ? 70 : 40,
-            mx: 'auto',
+            mx: "auto",
             my: 2,
           }}
         />
         <Typography
           variant="h2"
-          fontWeight={'bold'}
+          fontWeight={"bold"}
           fontSize={open ? 20 : 0}
           color="primary"
           margin={1}
         >
-          Ahmed Refai
+          {userData?.name}
         </Typography>
         <Typography variant="h3" fontSize={open ? 15 : 0} color="secondary">
-          Admin
+          {userData?.role}
         </Typography>
       </Box>
       <Divider />
--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
   TextField,
   FormControl,
@@ -9,25 +9,24 @@ import {
   Grid2,
   Box,
   Container,
-} from '@mui/material';
-import CustomButton from '@/components/Common/ButtonStyle';
-import { useFormik } from 'formik';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import * as Yup from 'yup';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import { CustomLink } from '@/components/Common/ButtonStyle';
-import LeftAuth from '../../../components/Common/LeftAuth';
-import { useTypeContext } from '../../../context/UserType.context';
-import FixedHead from '../../../components/Common/FixedHead';
+} from "@mui/material";
+import CustomButton from "@/components/Common/ButtonStyle";
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+import { CustomLink } from "@/components/Common/ButtonStyle";
+import LeftAuth from "../../../components/Common/LeftAuth";
+
+import FixedHead from "../../../components/Common/FixedHead";
+import { useTypeContext } from "../../../context/UserType.context";
 
 export default function Login() {
-  const { setToken, setRole } = useTypeContext();
+  const { login } = useTypeContext();
+
   const [showPassword, setShowPassword] = useState(false);
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordRegx = /^.{8,}$/;
-  const navigator = useNavigate();
+
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleMouseDownPassword = (event) => {
@@ -38,47 +37,22 @@ export default function Login() {
     event.preventDefault();
   };
 
-  async function login(values) {
-    const loading = toast.loading('watting');
-    try {
-      const options = {
-        url: 'https://pflow-api-v3-1655e5b56c39.herokuapp.com/api/v1/auth/login',
-        method: 'POST',
-        data: values,
-      };
-      const { data } = await axios.request(options);
-      console.log(data);
-      if (data.message === 'success') {
-        toast.success(data.message);
-        setRole(data.user.role);
-        setToken(data.token);
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('role', data.user.role);
-        navigator('/');
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error(error.response.data.message);
-    } finally {
-      toast.dismiss(loading);
-    }
-  }
   const validationSchema = Yup.object({
     email: Yup.string()
-      .required('Required')
-      .matches(emailRegex, 'Invalid email'),
+      .required("Required")
+      .matches(emailRegex, "Invalid email"),
     password: Yup.string()
-      .required('Required')
+      .required("Required")
       .matches(
         passwordRegx,
-        'Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special case character'
+        "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special case character"
       ),
   });
   const { handleBlur, handleChange, handleSubmit, errors, values, touched } =
     useFormik({
       initialValues: {
-        email: '',
-        password: '',
+        email: "",
+        password: "",
       },
       validationSchema,
       onSubmit: login,
@@ -89,8 +63,8 @@ export default function Login() {
       spacing={5}
       container
       sx={{
-        minHeight: '100vh',
-        width: '100%',
+        minHeight: "100vh",
+        width: "100%",
       }}
     >
       <LeftAuth namePage="Sign Up" path="/signup"></LeftAuth>
@@ -98,14 +72,14 @@ export default function Login() {
         size={{ xs: 12, md: 8 }}
         sx={{
           pt: 5,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
         }}
       >
         <Container maxWidth="sm">
           <FixedHead>Sign in to</FixedHead>
-          <Box component={'form'} sx={{ pt: 5 }} onSubmit={handleSubmit}>
+          <Box component={"form"} sx={{ pt: 5 }} onSubmit={handleSubmit}>
             <TextField
               fullWidth
               label="Email"
@@ -120,7 +94,7 @@ export default function Login() {
             />
             <FormControl fullWidth sx={{ mb: 8 }} variant="outlined">
               <InputLabel
-                color={errors.email && touched.email ? 'error' : 'primary'}
+                color={errors.email && touched.email ? "error" : "primary"}
                 htmlFor="outlined-adornment-password"
               >
                 Password
@@ -128,7 +102,7 @@ export default function Login() {
               <OutlinedInput
                 sx={{ mb: 2 }}
                 id="outlined-adornment-password"
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 name="password"
                 value={values.password}
                 onChange={handleChange}
@@ -140,8 +114,8 @@ export default function Login() {
                     <IconButton
                       aria-label={
                         showPassword
-                          ? 'hide the password'
-                          : 'display the password'
+                          ? "hide the password"
+                          : "display the password"
                       }
                       onClick={handleClickShowPassword}
                       onMouseDown={handleMouseDownPassword}
@@ -155,12 +129,12 @@ export default function Login() {
                 label="Password"
               />
               {/* <Box component={'div'} width={'fit-content'} align={'end'}> */}
-              <CustomLink bghover={true} to="/forgetpassword" ml={'auto'}>
+              <CustomLink bghover={true} to="/forgetpassword" ml={"auto"}>
                 Forgot Your Password ?
               </CustomLink>
               {/* </Box> */}
             </FormControl>
-            <Box component={'div'} mb={3}>
+            <Box component={"div"} mb={3}>
               <CustomButton type="submit" w="100%" sm="45%">
                 Login
               </CustomButton>
