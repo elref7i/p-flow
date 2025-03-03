4a52d46aa7eacaaa39f1befebaa23da768d6b0e7
elref7i <momo3956248@gmail.com>
2025-03-03 19:04:36 +0200

    Add password update validation schema and implement form handling in ChangePassword component

--- src/lib/schemas/UserSchema.js
+++ src/lib/schemas/UserSchema.js
@@ -1,31 +1,17 @@
 import * as Yup from 'yup';
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const phoneRegx = /^(02)?01[0125][0-9]{8}/;
 const passwordRegx = /^\d{8,}$/;
-
-const emailField = Yup.string()
-  .matches(emailRegex, 'Invalid email')
-  .required('Email is required');
 
 const passwordField = Yup.string()
   .matches(passwordRegx, 'Password must be exactly 8 digits') // ✅ تصحيح التحقق من الباسورد
   .required('Password is required');
 
-const confirmPasswordField = Yup.string()
-  .oneOf([Yup.ref('password'), null], 'Passwords must match')
-  .required('Confirm password is required');
-
 export const loggedUserSchema = Yup.object().shape({
   name: Yup.string().required('User name is required'),
-  //! email: emailField,
   ownerName: Yup.string().required('Owner name is required'),
   phone: Yup.string()
     .required('Phone is required')
     .matches(phoneRegx, 'Invalid phone number'),
-  //! registrationNumber: Yup.string().required('Registration number is required'),
-  //! identificationNumber: Yup.string().required(
-  //   'Identification number is required'
-  // ),
   city: Yup.string().required('City is required'),
   governorate: Yup.string().required('Governorate is required'),
   location: Yup.object()
@@ -36,4 +22,19 @@ export const loggedUserSchema = Yup.object().shape({
         .length(2, 'Coordinates must be an array of two numbers'),
     })
     .required('Loaction is Required'),
+});
+
+export const updatesLoggedUserPass = Yup.object({
+  oldPassword: Yup.string()
+    .required('Old password is required.')
+    .matches(passwordField, 'Old password must be at least 8 characters long.'),
+
+  newPassword: Yup.string()
+    .required('New password is required.')
+    .min(8, 'New password must be at least 8 characters long.')
+    .matches(passwordField, 'New password must be at least 8 characters long.')
+    .notOneOf(
+      [Yup.ref('oldPassword')],
+      'New password must be different from the old password.'
+    ),
 });
--- src/pages/Setting/ChangePassword.jsx
+++ src/pages/Setting/ChangePassword.jsx
@@ -1,5 +1,19 @@
 import { Box } from '@mui/material';
+import { useFormik } from 'formik';
+import { updatesLoggedUserPass } from '../../lib/schemas/UserSchema';
 
 export default function ChangePassword() {
+  // const f = { handleBlur, handleChange, handleSubmit, errors, touched, values };
+  const formik = useFormik({
+    initialValues: {
+      oldPassword: '',
+      newPassword: '',
+    },
+    validationSchema: updatesLoggedUserPass,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return <Box></Box>;
 }
