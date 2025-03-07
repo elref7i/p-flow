d9ae9d8bb3a04c9be492211fe85963a545b9def5
ascom <zyad16230@gmail.com>
2025-03-06 17:29:48 -0800

    Refactor inventory dashboard to include AddDrugFromExcel component and remove deprecated AddDrugComponent

--- src/pages/Inventory/DashboardInventory/DashboardInventory.jsx
+++ src/pages/Inventory/DashboardInventory/DashboardInventory.jsx
@@ -1,7 +1,13 @@
-import AddDrugComponent from "../../../components/AddDrugComponent/AddDrugComponent";
+import { Stack } from "@mui/material";
+import AddDrugComponent from "../../../components/InventoryComponents/AddDrugComponent/AddDrugComponent";
+import AddDrugFromExcel from "../../../components/InventoryComponents/AddDrugFromExcel/AddDrugFromExcel";
+
 
 export default function DashboardInventory() {
   return (
-    <AddDrugComponent/>
+    <Stack direction="row" spacing={2} justifyContent="flex-end">
+      <AddDrugComponent />
+      <AddDrugFromExcel />
+    </Stack>
   )
 }
