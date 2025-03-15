803ee5f162737ca770103f69ce5be7f10ed4793d
ascom <zyad16230@gmail.com>
2025-03-15 21:32:09 +0200

    Add AddDrugComponent and AddDrugFromExcel to AllDrugs page; remove unused components from DashboardInventory

--- src/pages/Inventory/AllDrugs/AllDrugs.jsx
+++ src/pages/Inventory/AllDrugs/AllDrugs.jsx
@@ -1,55 +1,51 @@
-import { columns } from './Drugs';
-import Table from '../../../components/InventoryComponents/Table/Table';
-import { useOwnDrugs } from '../../../lib/hooks/useDrugAction';
-
+import { columns } from "./Drugs";
+import Table from "../../../components/InventoryComponents/Table/Table";
+import { useOwnDrugs } from "../../../lib/hooks/useDrugAction";
+import { Stack } from "@mui/material";
+import AddDrugComponent from "../../../components/InventoryComponents/AddDrugComponent/AddDrugComponent";
+import AddDrugFromExcel from "../../../components/InventoryComponents/AddDrugFromExcel/AddDrugFromExcel";
 export default function AllDrugs() {
   const { data, isLoading } = useOwnDrugs();
-  
+
   // console.log("Fetched data:", data);
   const columnsWithActions = [...columns];
 
   return (
-    <Table
-      isLoading={isLoading}
-      data={data || []}
-      columnsWithActions={columnsWithActions}
-      check={false}
-    />
+    <>
+      <Stack direction="row" spacing={2} justifyContent="flex-end">
+        <AddDrugComponent />
+        <AddDrugFromExcel />
+      </Stack>
+      <Table
+        isLoading={isLoading}
+        data={data || []}
+        columnsWithActions={columnsWithActions}
+        check={false}
+      />
+    </>
   );
 }
 
+// const columnsWithActions = [PrfileImage, ...columns];
 
-
-
-
-
-
-
-
-
-
-  // const columnsWithActions = [PrfileImage, ...columns];
-
-
-
-  // const PrfileImage = {
-  //   field: "profileImage",
-  //   headerName: "PrfileImage",
-  //   align: "center",
-  //   headerAlign: "center",
-  //   minWidth: 150,
-  //   renderCell: (params) => {
-  //     return (
-  //       <Box
-  //         sx={{
-  //           pt: 1,
-  //           display: "flex",
-  //           alignItems: "center",
-  //           justifyContent: "center",
-  //         }}
-  //       >
-  //         <Avatar alt="Remy Sharp" src={params.row.profileImage} />
-  //       </Box>
-  //     );
-  //   },
-  // };
+// const PrfileImage = {
+//   field: "profileImage",
+//   headerName: "PrfileImage",
+//   align: "center",
+//   headerAlign: "center",
+//   minWidth: 150,
+//   renderCell: (params) => {
+//     return (
+//       <Box
+//         sx={{
+//           pt: 1,
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//         }}
+//       >
+//         <Avatar alt="Remy Sharp" src={params.row.profileImage} />
+//       </Box>
+//     );
+//   },
+// };
--- src/pages/Inventory/DashboardInventory/DashboardInventory.jsx
+++ src/pages/Inventory/DashboardInventory/DashboardInventory.jsx
@@ -1,13 +1,6 @@
-import { Stack } from "@mui/material";
-import AddDrugComponent from "../../../components/InventoryComponents/AddDrugComponent/AddDrugComponent";
-import AddDrugFromExcel from "../../../components/InventoryComponents/AddDrugFromExcel/AddDrugFromExcel";
-
 
 export default function DashboardInventory() {
   return (
-    <Stack direction="row" spacing={2} justifyContent="flex-end">
-      <AddDrugComponent />
-      <AddDrugFromExcel />
-    </Stack>
+    <div>DashboardInventory</div>
   )
 }
