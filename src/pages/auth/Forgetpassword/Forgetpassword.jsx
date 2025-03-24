84b666987946a16e765d75be13681fe6ca024d60
ascom <zyad16230@gmail.com>
2025-03-24 23:49:21 +0200

    Refactor authentication pages to utilize theme for background styling; enhance layout and structure for improved readability and user experience.

--- src/pages/auth/Forgetpassword/Forgetpassword.jsx
+++ src/pages/auth/Forgetpassword/Forgetpassword.jsx
@@ -1,34 +1,44 @@
-import { TextField, Typography, Grid2, Box, Container } from '@mui/material';
-import CustomButton from '@/components/Common/ButtonStyle';
-import { useFormik } from 'formik';
-import { CustomHead } from '@/components/Common/CustomTypography';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import LeftAuth from '@/components/Common/LeftAuth';
-import { useForgetPassword } from '@/context/Forget.context';
-import { forgetSchema } from '@/lib/schemas/AuthSchema';
+import {
+  TextField,
+  Typography,
+  Grid2,
+  Box,
+  Container,
+  useTheme,
+} from "@mui/material";
+import CustomButton from "@/components/Common/ButtonStyle";
+import { useFormik } from "formik";
+import { CustomHead } from "@/components/Common/CustomTypography";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { CustomLink } from "@/components/Common/ButtonStyle";
+import LoginIcon from "@mui/icons-material/Login";
+import { useForgetPassword } from "@/context/Forget.context";
+import { forgetSchema } from "@/lib/schemas/AuthSchema";
 
 export default function ForgetPassword() {
+  const theme = useTheme();
+  const backgroundAuth = theme.palette.background.auth;
   const navigator = useNavigate();
 
   const { setForgetCompleted } = useForgetPassword();
   async function forgetpassword(values) {
-    const loading = toast.loading('waiting...');
+    const loading = toast.loading("waiting...");
 
     try {
       const options = {
-        url: 'https://pflow-api-v3-1655e5b56c39.herokuapp.com/api/v1/auth/forgetpassword',
-        method: 'POST',
+        url: "https://pflow-api-v3-1655e5b56c39.herokuapp.com/api/v1/auth/forgetpassword",
+        method: "POST",
         data: values,
       };
       const { data } = await axios.request(options);
       // console.log(data.message);
-      if (data.message === 'Reset code sent successfully') {
+      if (data.message === "Reset code sent successfully") {
         toast.success(data.message);
         setForgetCompleted(true);
         setTimeout(() => {
-          navigator('/verifysendcoding');
+          navigator("/verifysendcoding");
         }, 2000);
       }
     } catch (error) {
@@ -42,72 +52,99 @@ export default function ForgetPassword() {
   const { handleBlur, handleChange, handleSubmit, errors, touched, values } =
     useFormik({
       initialValues: {
-        email: '',
+        email: "",
       },
       validationSchema: forgetSchema,
       onSubmit: forgetpassword,
     });
 
   return (
-    <Grid2
-      spacing={5}
-      container
+    <Box
+      component={"main"}
       sx={{
-        minHeight: '100vh',
-        width: '100%',
+        background: backgroundAuth, // لون ثابت
+        // backgroundImage: (theme) => theme.palette.background.authImage, // التدرج
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
       }}
     >
-      <LeftAuth namePage="Sign in" path="/login"></LeftAuth>
+      <Box sx={{ position: "absolute", top: 15, left: 50 }}>
+        <CustomLink
+          to={"/login"}
+          bghover={true}
+          bg={true}
+          fs={"30px"}
+          fw="bold"
+        >
+          <LoginIcon color="primary"></LoginIcon>
+        </CustomLink>
+      </Box>
       <Grid2
-        size={{ xs: 12, md: 8 }}
+        spacing={5}
+        container
         sx={{
-          bg: 'red',
-          pt: 5,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
+          minHeight: "100vh",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Container maxWidth={'sm'}>
-          <Box component={'header'} paddingBottom={4}>
-            <CustomHead variant="h1" align="left">
-              Forget Password
-            </CustomHead>
-            <Typography
-              variant="body1"
-              sx={{ fontSize: '15px', color: '#939494' }}
-            >
-              Please enter your registered email address to receive a password
-              reset link. If you don&apos;t remember your email address, contact
-              our support team for assistance.
-            </Typography>
-          </Box>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              fullWidth
-              label="Email"
-              margin="normal"
-              sx={{ mb: 3 }}
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={errors.email && touched.email}
-              helperText={touched.email && errors.email}
-            />
+        <Grid2
+          size={{ xs: 12, md: 8 }}
+          sx={{
+            bg: "red",
+            pt: 5,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Container maxWidth={"sm"}>
+            <Box component={"header"} paddingBottom={4}>
+              <CustomHead variant="h1" align="left">
+                Forget Password
+              </CustomHead>
+              <Typography
+                variant="body1"
+                sx={{ fontSize: "15px", color: "#939494" }}
+              >
+                Please enter your registered email address to receive a password
+                reset link. If you don&apos;t remember your email address,
+                contact our support team for assistance.
+              </Typography>
+            </Box>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                fullWidth
+                label="Email"
+                margin="normal"
+                sx={{ mb: 3 }}
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.email && touched.email}
+                helperText={touched.email && errors.email}
+              />
 
-            <CustomButton
-              type="submit"
-              // @ts-ignore
-              w="75%"
-              sm="75%"
-              md="50%"
-            >
-              Send
-            </CustomButton>
-          </form>
-        </Container>
+              <CustomButton
+                type="submit"
+                // @ts-ignore
+                w="75%"
+                sm="75%"
+                md="50%"
+              >
+                Send
+              </CustomButton>
+            </form>
+          </Container>
+        </Grid2>
       </Grid2>
-    </Grid2>
+    </Box>
   );
 }
--- src/pages/auth/UpdatedPassword/UpdatedPassword.jsx
+++ src/pages/auth/UpdatedPassword/UpdatedPassword.jsx
@@ -1,30 +1,40 @@
-import { Typography, Grid2, Box, TextField, Container } from '@mui/material';
-import CustomButton from '@/components/Common/ButtonStyle';
-import { useFormik } from 'formik';
-import { CustomHead } from '@/components/Common/CustomTypography';
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import LeftAuth from '@/components/Common/LeftAuth';
-import { updateAuthSchema } from '@/lib/schemas/AuthSchema';
-import PasswordControl from '@/components/Common/PasswordControl';
+import {
+  Typography,
+  Grid2,
+  Box,
+  TextField,
+  Container,
+  useTheme,
+} from "@mui/material";
+import CustomButton from "@/components/Common/ButtonStyle";
+import { useFormik } from "formik";
+import { CustomHead } from "@/components/Common/CustomTypography";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { CustomLink } from "@/components/Common/ButtonStyle";
+import HomeIcon from "@mui/icons-material/Home";
+import { updateAuthSchema } from "@/lib/schemas/AuthSchema";
+import PasswordControl from "@/components/Common/PasswordControl";
 
 export default function UpdatedPassword() {
+  const theme = useTheme();
+  const backgroundAuth = theme.palette.background.auth;
   const navigator = useNavigate();
 
   async function updatedPassword(values) {
-    const loading = toast.loading('waiting...');
+    const loading = toast.loading("waiting...");
     try {
       const options = {
-        url: 'https://pflow-api-v3-1655e5b56c39.herokuapp.com/api/v1/auth/resetPassword',
-        method: 'PUT',
+        url: "https://pflow-api-v3-1655e5b56c39.herokuapp.com/api/v1/auth/resetPassword",
+        method: "PUT",
         data: values,
       };
       const { data } = await axios.request(options);
       console.log(data);
-      if (data.message === 'success') {
+      if (data.message === "success") {
         toast.success(data.message);
-        navigator('/login');
+        navigator("/login");
       }
     } catch (error) {
       toast.error(error.response.data.message);
@@ -37,81 +47,108 @@ export default function UpdatedPassword() {
   const { handleBlur, handleChange, handleSubmit, errors, touched, values } =
     useFormik({
       initialValues: {
-        email: '',
-        newPassword: '',
+        email: "",
+        newPassword: "",
       },
       validationSchema: updateAuthSchema,
       onSubmit: updatedPassword,
     });
 
   return (
-    <Grid2
-      spacing={5}
-      container
+    <Box
+      component={"main"}
       sx={{
-        minHeight: '100vh',
-        width: '100%',
+        background: backgroundAuth, // لون ثابت
+        // backgroundImage: (theme) => theme.palette.background.authImage, // التدرج
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
       }}
     >
-      <LeftAuth />
       <Grid2
-        size={{ xs: 12, md: 8 }}
+        spacing={5}
+        container
         sx={{
-          bg: 'red',
-          pt: 5,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
+          minHeight: "100vh",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Container maxWidth="sm">
-          <Box component={'header'} marginBottom={3}>
-            <CustomHead variant="h1" align="left">
-              Update Your Password
-            </CustomHead>
-            <Typography
-              variant="body1"
-              sx={{ fontSize: '15px', color: '#939494' }}
-            >
-              Please create a new password for your account. Ensure it meets the
-              required criteria to keep your account secure.
-            </Typography>
-          </Box>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              fullWidth
-              label="Email"
-              margin="normal"
-              sx={{ mb: 3 }}
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={errors.email && touched.email}
-              helperText={touched.email && errors.email}
-            />
+        <Box sx={{ position: "absolute", top: 15, left: 50 }}>
+          <CustomLink
+            to={"/landing"}
+            bghover={true}
+            bg={true}
+            fs={"30px"}
+            fw="bold"
+          >
+            <HomeIcon color="primary"></HomeIcon>
+          </CustomLink>
+        </Box>
+        <Grid2
+          size={{ xs: 12, md: 8 }}
+          sx={{
+            bg: "red",
+            pt: 5,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Container maxWidth="sm">
+            <Box component={"header"} marginBottom={3}>
+              <CustomHead variant="h1" align="left">
+                Update Your Password
+              </CustomHead>
+              <Typography
+                variant="body1"
+                sx={{ fontSize: "15px", color: "#939494" }}
+              >
+                Please create a new password for your account. Ensure it meets
+                the required criteria to keep your account secure.
+              </Typography>
+            </Box>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                fullWidth
+                label="Email"
+                margin="normal"
+                sx={{ mb: 3 }}
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.email && touched.email}
+                helperText={touched.email && errors.email}
+              />
 
-            <PasswordControl
-              name="newPassword"
-              error={errors.newPassword}
-              value={values.newPassword}
-              touched={touched.newPassword}
-              handleBlur={handleBlur}
-              handleChange={handleChange}
-              text="New Password"
-            />
-            <CustomButton
-              type="submit"
-              // @ts-ignore
-              w="100%"
-              sm="50%"
-              md="40%"
-            >
-              Update
-            </CustomButton>
-          </form>
-        </Container>
+              <PasswordControl
+                name="newPassword"
+                error={errors.newPassword}
+                value={values.newPassword}
+                touched={touched.newPassword}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                text="New Password"
+              />
+              <CustomButton
+                type="submit"
+                // @ts-ignore
+                w="100%"
+                sm="50%"
+                md="40%"
+              >
+                Update
+              </CustomButton>
+            </form>
+          </Container>
+        </Grid2>
       </Grid2>
-    </Grid2>
+    </Box>
   );
 }
