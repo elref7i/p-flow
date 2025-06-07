7570dcbfe9ada4d209b10f905bb992581a7feeba
Ahmed Refai <momo3956248@gmail.com>
2025-06-07 21:19:46 +0300

    feat: Implement notifications API functions for adding, retrieving, and managing notifications

---
+++ src/lib/api/notification.api.js
@@ -0,0 +1,119 @@
+import axios from "axios";
+import { NOTIF_URL } from "./api_url";
+
+// Add  Notifications
+export async function addNotifications({ token, values }) {
+  const options = {
+    method: "POST",
+    url: NOTIF_URL,
+    data: { values },
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+//Get All Notifications
+export async function getAllNotifications({ token }) {
+  const options = {
+    method: "GET",
+    url: NOTIF_URL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+// Get Unread Notifications
+export async function getUnreadNotif({ token }) {
+  const options = {
+    method: "GET",
+    url: `${NOTIF_URL}/unread-count`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+// Add a Notification Mark
+export async function addMark({ token, notifId }) {
+  const options = {
+    method: "PATCH",
+    url: `${NOTIF_URL}/:${notifId}/read`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+// Add All Notifications Mark
+export async function addAllMark({ token }) {
+  const options = {
+    method: "PATCH",
+    url: `${NOTIF_URL}/read-all`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+// Delete a Notification
+export async function deleteNotif({ token, notifId }) {
+  const options = {
+    method: "DELETE",
+    url: `${NOTIF_URL}/${notifId}`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
+
+// Delete Expired Notifications
+export async function deleteExpiredNotif({ token }) {
+  const options = {
+    method: "DELETE",
+    url: `${NOTIF_URL}/expired`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  const { data } = await axios.request(options);
+  console.log(data);
+
+  return data;
+}
